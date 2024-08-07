69b69dcee82007f20718e0c22ef57b8906db51f7
guoyikai <guoyikai@autohome.com.cn>
2024-08-07 15:13:07 +0800

    加载实现动画

--- app/src/main/java/com/example/feedbackapp/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/feedbackapp/adapter/PhotoAdapter.kt
@@ -3,13 +3,20 @@ package com.example.feedbackapp.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.feedbackapp.R
 import com.example.feedbackapp.activity.AgentWebViewActivity
 import com.example.feedbackapp.activity.WatchPicActivity
@@ -27,6 +34,7 @@ class PhotoAdapter(private val context: Context) : RecyclerView.Adapter<PhotoAda
     inner class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val photoImageView:ImageView = itemView.findViewById(R.id.photoImageView)
         val isVideoIv:ImageView = itemView.findViewById(R.id.is_video_iv)
+        val progressBar:ProgressBar = itemView.findViewById(R.id.progressBar)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
@@ -55,14 +63,58 @@ class PhotoAdapter(private val context: Context) : RecyclerView.Adapter<PhotoAda
             Glide.with(context)
                 .asBitmap()
                 .load(photoUrl)
+                .listener(object :RequestListener<Bitmap>{
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Bitmap?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    holder.progressBar.visibility = View.GONE
+                    return false
+                }
+
+            })
                 .thumbnail(0.1f) // 加载视频的缩略图，0.1f表示加载10%的帧数
                 .into(holder.photoImageView)
             holder.isVideoIv.visibility = View.VISIBLE
         } else {
             Glide.with(context)
                 .load(photoUrl)
-                .placeholder(R.drawable.loading)
+                .listener(object :RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        holder.progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                })
                 .into(holder.photoImageView)
+
             holder.isVideoIv.visibility = View.GONE
         }
 
